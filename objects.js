bade86907f4ac6197ca28ff3ded6989bae191a75
Avinash Nepal <nepalavinash@gmail.com>
2019-12-05 18:59:04 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { metallica: "one" };
+
+playlist.artist = "metallica"
+playlist.song = "one"
+var updatePlaylist = new playlist( metallica, one);
+var updatePlaylist = {
+  playlist : "metallica",
+  artistName : "james",
+  songtitle : "one"
+};
